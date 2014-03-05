b366b68d92a601af9958e1278e69c5f92500fa64
sandramh <sandy005495@gmail.com>
2014-03-05 10:38:02 -0600

    primer commit

---
+++ src/javaapplication1/JavaApplication1.java
@@ -0,0 +1,27 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication1;
+
+/**
+ *
+ * @author ussuario
+ */
+public class JavaApplication1 {
+    
+    
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+      
+
+			
+		System.out.println("Hola Mundo");
+	}
+      
+    }
+
